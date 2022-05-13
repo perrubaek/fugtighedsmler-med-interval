ad16df72652f226f724dfd268403ba049b367872
perrubaek <105459652+perrubaek@users.noreply.github.com>
2022-05-13 10:20:32 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 let fugt = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P0, 1023)
     basic.pause(500)
@@ -8,6 +15,15 @@ basic.forever(function () {
     )
     basic.pause(500)
     pins.analogWritePin(AnalogPin.P0, 0)
+    if (fugt < 900) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
     basic.pause(6000)
 })
 basic.forever(function () {

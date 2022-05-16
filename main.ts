ddde91336aaa1711b906f0ed45dfb309f2cd7fb9
perrubaek <105459652+perrubaek@users.noreply.github.com>
2022-05-16 11:55:25 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,13 +11,14 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P0, 1023)
-    basic.pause(500)
+    basic.pause(2000)
+    fugt = pins.analogReadPin(AnalogPin.P0)
     led.plotBarGraph(
     fugt,
     1023
     )
+    basic.showNumber(fugt)
     basic.pause(500)
-    pins.analogWritePin(AnalogPin.P0, 0)
     if (fugt < 900) {
         basic.showLeds(`
             # . . . #
@@ -27,8 +28,9 @@ basic.forever(function () {
             # . . . #
             `)
     }
+    pins.analogWritePin(AnalogPin.P0, 0)
     basic.pause(6000)
 })
 basic.forever(function () {
-    fugt = pins.analogReadPin(AnalogPin.P0)
+	
 })
